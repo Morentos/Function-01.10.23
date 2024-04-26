e285b45bad9eb8f3629bc78142d6ae444b10c8ed
Morentos <166997688+Morentos@users.noreply.github.com>
2024-04-26 15:42:20 +0300

    Add files via upload

--- Function 01.10.23.py
+++ Function 01.10.23.py
@@ -1,6 +1,6 @@
 def print_params(name):  #Создайте функцию def print_params
     print(name, name)  # в своем теле будет распечатывать переданное значение из параметра 2 раза!
 
-print_params('Max')
+print_params('Max')  # Вызовите данную функцию несколько раз в том же файле
 print_params('Anrew')
 print_params('Max is an excellent programmer')
